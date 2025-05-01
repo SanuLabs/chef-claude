c20f1ecbbd2857559bcb8f9eba321595033e8446
SanuLabs <muhammedsiyam868@gmail.com>
2025-05-01 20:36:14 +0530

    Initial commit & Header design

---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return(
+        <div className="header">
+         <img src="../images/chef-claude-icon.png" alt="Chef Claude" className="header-image"/>
+         <span className="header-title">Chef Claude</span>
+
+        </div>
+    )
+}
