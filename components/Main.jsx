cc594c2515900094abad91bccd9aa890292f2562
SanuLabs <muhammedsiyam868@gmail.com>
2025-06-12 01:25:02 +0530

    fix empty bullet

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,16 +1,31 @@
+import React from "react"
 export default function Main(){
+    const [ingredients, setNewIngredients] = React.useState([])
+    const ingredientList = ingredients.map(ingredient => 
+        <li key={ingredient}>{ingredient}</li>
+    )
+
+    function addIngredient(formData){
+        const newingredient = formData.get("ingredient")
+     if(newingredient!=""){setNewIngredients(prevIngredients => [...prevIngredients, newingredient])}
+    }
     return(
         <div className="main">
-            <form className="add-ingredient-form">
+            <form className="add-ingredient-form" action={addIngredient}>
                 <input aria-label="Add ingredient"
                 type="text"
                 placeholder="e.g. oregano"
                 className="form-input"
+                name="ingredient"
                 />
                 <button className="add-ingredient-button">
                     Add ingredient
                 </button>
-            </form>
+            </form> 
+            <ul>
+
+                {ingredientList}
+            </ul>
         </div>
     )
 }
