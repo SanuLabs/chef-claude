6dd3ba1e794228b975800c7439164d89b0be29d2
SanuLabs <muhammedsiyam868@gmail.com>
2025-06-15 17:21:53 +0530

    Implement ai suggestions based on MistralAI model via HuggingFace

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,11 +1,15 @@
 import React from "react"
 import ClaudeRecipe from "./ClaudeRecipe"
 import IngredientsList from "./IngredientsList"
+import { getRecipeFromMistral } from "../ai"
 export default function Main(){
     const [ingredients, setNewIngredients] = React.useState([])
-    const [recipeShown, setRecipeShown] = React.useState(false)
-    function toggleRecipeShown(){
-        setRecipeShown(prevShown => !prevShown)
+    const [recipe, setRecipe] = React.useState("")
+    async function getRecipe(){
+        console.log("WORKING")
+        const recipeMarkdown = await getRecipeFromMistral(ingredients)
+        setRecipe(recipeMarkdown)
+
     }
    
     
@@ -26,8 +30,8 @@ export default function Main(){
                     Add ingredient
                 </button>
             </form> 
-            {ingredients.length>0 && <IngredientsList ingredients={ingredients} toggleRecipeShown={toggleRecipeShown} />}
-            {recipeShown && <ClaudeRecipe/>}
+            {ingredients.length>0 && <IngredientsList ingredients={ingredients} getRecipe={getRecipe} />}
+            {recipe && <ClaudeRecipe recipe={recipe}/>}
         </div>
     )
 }
